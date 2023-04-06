d8b61f480a41614eaa6e9fad0df5f2e864d2bff4
Dephney Mokabane <34334152+Dephney@users.noreply.github.com>
2023-04-06 06:47:08 +0000

    Added dynamic pages for each job

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, jsonify  # from the module flask theres a class called Flask we're importing
-from database import load_jobs_from_db
+from database import load_jobs_from_db, load_job_from_db
 
 app = Flask(
   __name__)  # creating object of type Flask class == creating flask appication
@@ -11,7 +11,16 @@ def hello_world():
   return render_template("home.html", jobs=jobs)
 
 
-# @app.route("/api/jobs")  #json enpoint/api route
+# display a job with a specific job id
+@app.route("/job/<id>")
+def show_job(id):
+  job = load_job_from_db(id)
+  if not job:
+    return "Not Found", 404
+  return render_template("jobpage.html", job=job)
+
+
+# @app.route("/api/jobs")  #json endpoint/api route
 #def list_jobs():
 #  jobs = load_jobs_from_db()
 #  return jsonify(jobs)  # error - TypeError: Object of type Row is not JSON serializable
